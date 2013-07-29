f83ec1fc2de6af9b11290df84872ab45a2b181d6
tenfour <carlco@gmail.com>
2013-07-29 14:26:15 +0200

    Initial Commit

---
+++ TLStopwatch/TLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TLAppDelegate.h
+//  TLStopwatch
+//
+//  Created by Carl on 29/07/13.
+//  Copyright (c) 2013 Carl. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface TLAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ TLStopwatch/TLAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  TLAppDelegate.m
+//  TLStopwatch
+//
+//  Created by Carl on 29/07/13.
+//  Copyright (c) 2013 Carl. All rights reserved.
+//
+
+#import "TLAppDelegate.h"
+
+@implementation TLAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+	// Insert code here to initialize your application
+}
+
+@end
